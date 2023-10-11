29bd636f23c561c393b7c20aad60dc737dae918b
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-10-11 20:29:32 +0300

    Сверстать header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import icon from './icon.svg';
+
 function App() {
-  
-  return (
-    <div className="wrapper">
-      {/* <div class="grain">
-      </div> */}
-    </div>
-  );
+    return (
+        <div className="grain">
+            <div className="wrapper">
+                <header className="header">
+                    <div className="header__left">
+                        <img className="header__logo" src="/img/logo.svg"></img>
+                        <div className="header__info">
+                            <h3>SNEAKERSHOP</h3>
+                            <p>Магазин кросовок</p>
+                        </div>
+                    </div>
+
+                    <ul className="header__right">
+                        <li>
+                            <img src="/img/cart.svg"></img>
+                            <span>1205 руб.</span>
+                        </li>
+                        <li><img src="/img/account.svg"></img>
+                        <span>Профиль</span>
+                        </li>
+                    </ul>
+                </header>
+                <main className="main">
+                    {/* <h1>Все кросовки</h1> */}
+                </main>
+            </div>
+        </div>
+    );
 }
 
 export default App;
