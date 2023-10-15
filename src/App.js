e656c4cfd911c0919d33b7f8f5dfd341f6fe6c17
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-10-15 12:04:58 +0300

    Сверстать корзину

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 function App() {
     return (
         <>
+            <div className="overlay">
+                <div className="cart">
+                    <h2>Корзина</h2>
+                    <div className="cart__item">
+                        <img className="cart__item-img" src='./img/sneakers/NB990v3.jpg' alt="New Balance 990v3"></img>
+                        <div className="cart__item-info">
+                            <p>New Balance 990 v3 </p>
+                            <span>87 990 руб.</span>
+                        </div>
+                        <button className="cart__item-delete-btn"><img src="./img/delete.svg"></img></button>
+                    </div>
+                    <div className="cart__item">
+                        <img className="cart__item-img" src='./img/sneakers/NB990v3.jpg' alt="New Balance 990v3"></img>
+                        <div className="cart__item-info">
+                            <p>New Balance 990 v3 </p>
+                            <span>87 990 руб.</span>
+                        </div>
+                        <button className="cart__item-delete-btn"><img src="./img/delete.svg"></img></button>
+                    </div>
+                    <div className="cart__bottom">
+                        <div className="cart__total-price">
+                            <span>Итог</span>
+                            <span className="cart__price">89 990 руб.</span>
+                        </div>
+                        <div className="cart__bottom-btn-wrapper"><button className="cart__bottom-btn">Оформить заказ</button></div>
+                    </div>
+                </div>
+            </div>
             <div className="grain"></div>
             <div className="wrapper">
                 <header className="header">
@@ -36,7 +64,9 @@ function App() {
                     </div>
                     <div className="cards">
                         <div className="cards__item">
-                            <div className="card__item-favorit"><img src="./img/heart-unliked.svg"></img></div>
+                            <div className="card__item-favorit">
+                                <img src="./img/heart-unliked.svg"></img>
+                            </div>
                             <img src="./img/sneakers/NB990v3.jpg"></img>
                             <p className="cards__item-brand">New Balance</p>
                             <p className="cards__item-model">
@@ -52,7 +82,9 @@ function App() {
                             </div>
                         </div>
                         <div className="cards__item">
-                        <div className="card__item-favorit"><img src="./img/heart-unliked.svg"></img></div>
+                            <div className="card__item-favorit">
+                                <img src="./img/heart-unliked.svg"></img>
+                            </div>
                             <img src="./img/sneakers/NB990v5.jpg"></img>
                             <p className="cards__item-brand">New Balance</p>
                             <p className="cards__item-model">
@@ -68,11 +100,13 @@ function App() {
                             </div>
                         </div>
                         <div className="cards__item">
-                        <div className="card__item-favorit"><img src="./img/heart-unliked.svg"></img></div>
+                            <div className="card__item-favorit">
+                                <img src="./img/heart-unliked.svg"></img>
+                            </div>
                             <img src="./img/sneakers/NB990v3.jpg"></img>
                             <p className="cards__item-brand">Brand</p>
                             <p className="cards__item-model">Name Model</p>
-                            <div class="cards__item-bottom">
+                            <div className="cards__item-bottom">
                                 <span className="cards__item-price">
                                     12 990 руб.
                                 </span>
@@ -82,7 +116,9 @@ function App() {
                             </div>
                         </div>
                         <div className="cards__item">
-                        <div className="card__item-favorit"><img src="./img/heart-unliked.svg"></img></div>
+                            <div className="card__item-favorit">
+                                <img src="./img/heart-unliked.svg"></img>
+                            </div>
                             <img src="./img/sneakers/NB990v3.jpg"></img>
                             <p className="cards__item-brand">Brand</p>
                             <p className="cards__item-model">Name Model</p>
